10fc9d8b8c8d5c35fb2879fa6bdb75e672a182e0
Nazih <nazih.altajar@fortech.ro>
2019-03-19 09:57:58 +0200

    Chapter9

---
+++ Chapter9/Chapter9/RestaurantTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RestaurantTableViewCell.swift
+//  Chapter9
+//
+//  Created by Nazih Al Tajar on 15/03/2019.
+//  Copyright © 2019 sss. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+}
